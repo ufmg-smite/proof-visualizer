516a5738f2b00846128f2b5bedf77b42273d609f
Diego Della Rocca de Camargos <diegodellarocc@gmail.com>
2021-03-24 20:16:42 -0300

    refactor: Navbar as function

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
-export default class MyNavbar extends Component {
-  render() {
-    return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">Proof Visualizer</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/" className="nav-link">
-              Proofs
-            </Nav.Link>
-            <Nav.Link href="/create" className="nav-link">
-              New Proof
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+export default function MyNavbar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">Proof Visualizer</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/" className="nav-link">
+            Proofs
+          </Nav.Link>
+          <Nav.Link href="/create" className="nav-link">
+            New Proof
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
