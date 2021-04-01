a9eec5dbdbb97e509e6bad6a48135f18e381209b
Diego Della Rocca de Camargos <diegodellarocc@gmail.com>
2021-04-01 09:55:33 -0300

    refactor: Visualize as hooks

--- frontend/src/components/VisualizeProof.js
+++ frontend/src/components/VisualizeProof.js
@@ -1,87 +1,54 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import Canvas from '../graphic-components/Canvas';
 
-export default class VisualizeProof extends Component {
-  constructor(props) {
-    super(props);
+export default function VisualizeProof(props) {
+  const { location } = props;
+  const [dot, setDot] = useState(location.state.dot);
+  const [label, setLabel] = useState(location.state.label);
+  const [currentText, setCurrentText] = useState(
+    'right-click in a node to show the text here'
+  );
+  const [textOfFocusNode, setTextOfFocusNode] = useState('');
 
-    const { location } = this.props;
-
-    let dot = null;
-    let label = null;
-
-    if (location.state) {
-      dot = location.state.dot;
-      label = location.state.label;
-    }
-
-    this.setCurrentText = this.setCurrentText.bind(this);
-    this.setFocusText = this.setFocusText.bind(this);
-
-    this.state = {
-      dot,
-      label,
-      currentText: 'right-click in a node to show the text here',
-      textOfFocusNode: '',
-    };
-  }
-
-  componentDidMount() {
-    const { location } = this.props;
+  useEffect(() => {
     if (location.state) return;
     const proofId = location.pathname.split('/').slice(-1)[0];
     axios
       .get(`http://localhost:5000/proof/${proofId}`)
       .then((response) => {
-        const { dot, label } = JSON.parse(response.request.response);
-        this.setState({
-          dot,
-          label,
-        });
+        const { dotCp, labelCp } = JSON.parse(response.request.response);
+        setDot(dotCp);
+        setLabel(labelCp);
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  }, []);
 
-  setCurrentText(text) {
-    this.setState({
-      currentText: text,
-    });
-  }
+  const canvasContainerProps =
+    textOfFocusNode !== '' ? { title: textOfFocusNode } : {};
 
-  setFocusText(text) {
-    this.setState({
-      textOfFocusNode: text,
-    });
-  }
-
-  render() {
-    const { label, dot, currentText, textOfFocusNode } = this.state;
-    const props = textOfFocusNode !== '' ? { title: textOfFocusNode } : {};
-
-    return (
-      <div className="visualizer">
-        <h3 className="proof-name">{label}</h3>
-        {dot ? (
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          <div className="canvas-container" {...props}>
-            {' '}
-            <Canvas
-              dot={dot}
-              setCurrentText={this.setCurrentText}
-              setFocusText={this.setFocusText}
-            />
-          </div>
-        ) : null}
-        <div className="node-text">
-          <p>{currentText}</p>
+  return (
+    <div className="visualizer">
+      <h3 className="proof-name">{label}</h3>
+      {dot ? (
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        <div className="canvas-container" {...canvasContainerProps}>
+          {' '}
+          <Canvas
+            dot={dot}
+            setCurrentText={setCurrentText}
+            setFocusText={setTextOfFocusNode}
+          />
         </div>
+      ) : null}
+      <div className="node-text">
+        <p>{currentText}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 VisualizeProof.propTypes = {
