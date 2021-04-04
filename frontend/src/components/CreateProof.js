bc563090bbaee5f7aa04fdbf7cb9cec356eb976a
Diego Della Rocca de Camargos <diegodellarocc@gmail.com>
2021-04-04 17:32:33 -0300

    refactor: CreateProof using Hooks

--- frontend/src/components/CreateProof.js
+++ frontend/src/components/CreateProof.js
@@ -1,86 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Form, Button } from 'react-bootstrap';
 
-export default class CreateProof extends Component {
-  constructor(props) {
-    super(props);
+export default function CreateProof() {
+  const [label, setLabel] = useState('');
+  const [problem, setProblem] = useState('');
+  const [options, setOptions] = useState('');
 
-    this.onSubmit = this.onSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-
-    this.state = {
-      label: '',
-      problem: '',
-      inputLanguage: '',
-      options: '',
-    };
-  }
-
-  handleChange(evt) {
-    this.setState({ [evt.target.name]: evt.target.value });
-  }
-
-  async onSubmit(e) {
+  const onSubmit = async (e) => {
     e.preventDefault();
-
-    let { label, problem, inputLanguage, id, options } = this.state;
 
     const proof = {
       label,
       problem,
-      inputLanguage,
       options,
     };
     await axios
       .post('http://localhost:5000/proof/add/', proof)
       .then((res) =>
-        this.setState({ id: res.data }, () => ({ id } = this.state))
-      );
-    await axios.get(`http://localhost:5000/proof/process-proof/${id}`);
-    window.location = '/';
-  }
+        axios.get(`http://localhost:5000/proof/process-proof/${res.data}`)
+      )
+      .then(() => (window.location = '/'));
+  };
 
-  render() {
-    return (
-      <Form onSubmit={this.onSubmit}>
-        <Form.Group>
-          <Form.Label>Proof name</Form.Label>
-          <Form.Control
-            name="label"
-            type="text"
-            placeholder="proof-a-and-not-a"
-            onChange={this.handleChange}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>CVC4 Options</Form.Label>
-          <Form.Control
-            name="options"
-            type="text"
-            placeholder="Default options: --proof --dump-proof --proof-format-mode=dot"
-            onChange={this.handleChange}
-          />
-        </Form.Group>
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group>
+        <Form.Label>Proof name</Form.Label>
+        <Form.Control
+          name="label"
+          type="text"
+          placeholder="proof-a-and-not-a"
+          value={label}
+          onChange={(evt) => setLabel(evt.target.value)}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>CVC4 Options</Form.Label>
+        <Form.Control
+          name="options"
+          type="text"
+          placeholder="Default options: --proof --dump-proof --proof-format-mode=dot"
+          value={options}
+          onChange={(evt) => setOptions(evt.target.value)}
+        />
+      </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Problem</Form.Label>
-          <Form.Control
-            name="problem"
-            as="textarea"
-            rows={10}
-            placeholder="(set-logic QF_UF)
+      <Form.Group>
+        <Form.Label>Problem</Form.Label>
+        <Form.Control
+          name="problem"
+          as="textarea"
+          rows={10}
+          placeholder="(set-logic QF_UF)
 (declare-fun a () Bool)
 (assert (not a))
 (assert a)
 (check-sat)"
-            onChange={this.handleChange}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Generate proof
-        </Button>
-      </Form>
-    );
-  }
+          value={problem}
+          onChange={(evt) => setProblem(evt.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Generate proof
+      </Button>
+    </Form>
+  );
 }
