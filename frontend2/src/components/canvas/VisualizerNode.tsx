8b87282b1c86e3d6cbd976866c652c76f4ffb2ac
Diego Della Rocca de Camargos <diegodellarocc@gmail.com>
2021-05-19 14:52:01 -0300

    refactor: Node

---
+++ frontend2/src/components/canvas/VisualizerNode.tsx
@@ -0,0 +1,126 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import { Label, Text, Tag, Group } from 'react-konva';
+
+function textColorFromBg(bgColor: string) {
+    const color = bgColor.charAt(0) === '#' ? bgColor.substring(1, 7) : bgColor;
+    const r = parseInt(color.substring(0, 2), 16);
+    const g = parseInt(color.substring(2, 4), 16);
+    const b = parseInt(color.substring(4, 6), 16);
+    return r * 0.299 + g * 0.587 + b * 0.114 > 150 ? '#000000' : '#ffffff';
+}
+
+interface onClickArgs {
+    id: number;
+    x: number;
+    y: number;
+}
+
+interface NodeProps {
+    rule: string;
+    conclusion: string;
+    id: number;
+    onClick: (arg0: onClickArgs) => void;
+    setFocusText: Dispatch<SetStateAction<string>>;
+    setCurrentText: Dispatch<SetStateAction<string>>;
+    showingChildren: boolean;
+    updateNodeState: (key: number, x: number, y: number) => void;
+    x: number;
+    y: number;
+    hasChildren: boolean;
+    piNode: boolean;
+    setNodeOnFocus: (id: number) => void;
+}
+
+const Node = ({
+    rule,
+    conclusion,
+    id,
+    onClick,
+    setCurrentText,
+    setFocusText,
+    showingChildren,
+    x,
+    y,
+    hasChildren,
+    piNode,
+    setNodeOnFocus,
+    updateNodeState,
+}: NodeProps): JSX.Element => {
+    const bgClosedColor = '#2b2d42';
+    const bgOpenColor = '#8d99ae';
+
+    const bgColor = showingChildren || !hasChildren ? bgOpenColor : bgClosedColor;
+
+    return (
+        <Group
+            draggable
+            id={id.toString()}
+            key={id}
+            onDragMove={(e) => {
+                updateNodeState(id, e.target.attrs.x, e.target.attrs.y);
+            }}
+            x={x}
+            y={y}
+            onClick={(e) => {
+                if (e.evt.button === 2 && piNode) {
+                    setNodeOnFocus(id);
+                    const menuNode = document.getElementById('menu');
+                    if (menuNode) {
+                        menuNode.style.top = `${e.evt.clientY}px`;
+                        menuNode.style.left = `${e.evt.clientX}px`;
+                        menuNode.style.display = 'initial';
+                        window.addEventListener('click', () => {
+                            menuNode.style.display = 'none';
+                        });
+                    } else {
+                        // Apagar depois que garantido que o menu está ok
+                        alert('Problema com menu do canvas');
+                    }
+                }
+            }}
+        >
+            <Label
+                onClick={(e) => (e.evt.button === 2 ? setCurrentText(e.target.attrs.text) : onClick({ id, x, y }))}
+                onMouseEnter={(e) => {
+                    setFocusText(e.target.attrs.text);
+                }}
+                onMouseLeave={() => setFocusText('')}
+                x={0}
+                y={0}
+            >
+                <Tag fill={bgColor} stroke="black" />
+                <Text
+                    align="center"
+                    fill={textColorFromBg(bgColor)}
+                    fontSize={15}
+                    height={35}
+                    padding={10}
+                    text={conclusion}
+                    width={300}
+                />
+            </Label>
+            <Label
+                onClick={(e) => (e.evt.button === 2 ? setCurrentText(e.target.attrs.text) : onClick({ id, x, y }))}
+                x={0}
+                y={35}
+                onMouseEnter={(e) => {
+                    setFocusText(e.target.attrs.text);
+                }}
+                onMouseLeave={() => setFocusText('')}
+            >
+                <Tag fill={bgColor} stroke="black" />
+                <Text
+                    align="center"
+                    fill={textColorFromBg(bgColor)}
+                    fontSize={15}
+                    height={35}
+                    padding={10}
+                    text={rule}
+                    width={300}
+                />
+            </Label>
+        </Group>
+    );
+};
+
+export default Node;
