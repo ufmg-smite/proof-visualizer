022ba56dc49bccc7a0f539769ceff6e40894970f
vinciusb <65973642+vinciusb@users.noreply.github.com>
2021-11-04 19:40:42 -0300

    Update proofSlice.ts

--- frontend/src/features/proof/proofSlice.ts
+++ frontend/src/features/proof/proofSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
-import { processDot, piNodeChildren, piNodeParents, descendants } from './aux';
+import { processDot, piNodeChildren, piNodeParents, descendants } from './auxi';
 
 export interface NodeInterface {
     id: number;
