7394d8a420da77cf178926122a11c7ea094def34
vinciusb <65973642+vinciusb@users.noreply.github.com>
2022-04-11 08:13:44 -0300

    Created theory lemma visualizer

---
+++ frontend/src/components/VisualizerTheoryLemma/VisualizerTheoryLemma.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Classes } from '@blueprintjs/core';
+
+const VisualizerTheoryLemma: React.FC = () => {
+    return <div className={Classes.DIALOG_BODY}>aaa</div>;
+};
+
+export default VisualizerTheoryLemma;
