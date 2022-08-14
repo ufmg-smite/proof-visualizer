828102b10ace2e6d369a29d5d1a88ac86af7c482
vinciusb <65973642+vinciusb@users.noreply.github.com>
2022-08-14 10:19:23 -0300

    Pre pi node indexation fix

--- frontend/src/interfaces/undoClasses.ts
+++ frontend/src/interfaces/undoClasses.ts
@@ -29,18 +29,15 @@ export class ColorUndo extends BaseUndo {
 export class UnfoldUndo extends BaseUndo {
     positions: { [id: number]: { x: number; y: number } };
     colors: { [id: number]: string };
-    hiddenId: number;
 
     constructor(
         nodes: number[],
         positions: { [id: number]: { x: number; y: number } },
         colors: { [id: number]: string },
-        hiddenId: number,
     ) {
         super(nodes);
         this.positions = positions;
         this.colors = colors;
-        this.hiddenId = hiddenId;
     }
 }
 
